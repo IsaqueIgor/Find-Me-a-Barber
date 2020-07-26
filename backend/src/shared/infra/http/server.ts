d7a26c32e7996cd5fd1e670063df50a9c797b544
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-07-26 11:55:09 -0300

    fix: rate limiter on files

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,35 +1,42 @@
-import 'reflect-metadata';
-import 'dotenv/config';
-import cors from 'cors';
-import { errors } from 'celebrate';
-import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
+import "reflect-metadata";
+import "dotenv/config";
 
-import '../typeorm';
-import routes from './routes';
-import AppError from '../../errors/AppError';
+import cors from "cors";
+import { errors } from "celebrate";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+
+import uploadConfig from "@config/upload";
+import AppError from "@shared/errors/AppError";
+import rateLimiter from "./middlewares/rateLimiter";
+import routes from "./routes";
+
+import "@shared/infra/typeorm";
+import "@shared/container";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
 
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
-      status: 'error',
+      status: "error",
       message: err.message,
     });
   }
   console.error(err);
   return response.status(500).json({
-    status: 'error',
-    message: 'Internal Server Error',
+    status: "error",
+    message: "Internal Server Error",
   });
 });
 
 app.listen(3333, () => {
-  console.log('🚀 Server launched! Port 3333');
+  console.log("🚀 Server launched! Port 3333");
 });
