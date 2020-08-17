7e52d194cfb8d8c43733a9bb31448f22f9955eb3
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-08-17 16:53:14 -0300

    adding a few changes to dashboard

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -1,33 +1,44 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+:root {
+      --primary: #1A1E22;
+      --secondary: #D8D2CC;
+      --green: #038f3c;
+      --dark-green: #02171C;
+  }
   *{
     margin: 0;
     padding: 0;
     outline: 0;
     box-sizing: border-box;
   }
-  body{
-    background: var(--primary);
-    color: #FFF;
-    -webkit-font-smoothing: antialiased !important;
+    *:focus {
+    outline: 0;
   }
-  body, input, button{
-    font-family: 'Roboto Slab', sans-serif;
+  html, body, #root {
+    height: 100%;
+  }
+  body {
+    background-color: var(--primary);
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input, button {
+    font-family: "Roboto Slab", serif;
     font-size: 16px;
-    font-weight: normal;
   }
-  h1, h2, h3, h4, h5, h6, strong{
+  h1, h2, h3, h4, h5, h6, strong {
     font-weight: 500;
   }
-  button{
+  button {
     cursor: pointer;
   }
-
-  :root {
-      --primary: #1A1E22;
-      --secondary: #D8D2CC;
-      --green: #038f3c;
-      --dark-green: #02171C;
+  a {
+    text-decoration: none;
   }
+  ul {
+    list-style: none;
+  }
+
 `;
